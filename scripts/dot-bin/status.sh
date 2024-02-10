ff4f954be5a127b91262f0d8a8b2593aad49d67f
Anton Konyahin <me@konyahin.xyz>
2024-02-10 20:54:12 +0300

    scripts: add active task in status bar

--- scripts/dot-bin/status.sh
+++ scripts/dot-bin/status.sh
@@ -1,15 +1,22 @@
 #!/usr/bin/env sh
 
+clean_task () {
+    echo "$1" | sed -E 's/^[0-9]+-[0-9]+-[0-9]+ //' |
+        sed -E 's/\+active//' |
+        sed -E 's/ \@[[:alpha:]]+//'
+}
+
 while true; do
     LAYOUT=$(setxkbmap -query | grep layout | awk '{print $2}')
     CHARGE=$(apm -l)
     DATE=$(date '+ %H:%M %d-%m-%Y')
 
-    SONG=$(mpc current)
-    if [ -n "$SONG" ]; then
-	SONG="🎧 $SONG "
-    fi
+    #SONG=$(mpc current)
+    [ -n "$SONG" ] && SONG="🎧 $SONG "
 
-    xsetroot -name "$SONG🔋$CHARGE% $LAYOUT $DATE"
+    TASK=$(grep "+active" /home/anton/data/notes/todo.txt)
+    [ -n "$TASK" ] && TASK="🔵 $(clean_task "$TASK")"
+
+    xsetroot -name "$TASK $SONG🔋$CHARGE% $LAYOUT $DATE"
     sleep 0.2
 done
