b0359ff585ff5b66ae76db97487aff2c2d8aeff5
Anton Konyahin <me@konyahin.xyz>
2026-02-17 12:13:26 +0300

    emacs: go-mode and lsp

--- emacs/.emacs.d/early-init.el
+++
@@ -1,9 +0,0 @@
-;; -*- lexical-binding: t; -*-
-
-(set-face-attribute 'default nil :height 150)
-
-(menu-bar-mode -1)
-
-(when (display-graphic-p)
-  (tool-bar-mode -1)
-  (scroll-bar-mode -1))
--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,25 +1,37 @@
 ;; -*- lexical-binding: t; -*-
 
-(setq custom-file "~/.emacs.d/custom.el")
+(use-package emacs
+  :init (setq use-short-answers t
+	      custom-file "~/.emacs.d/custom.el"
+	      make-backup-files nil
+	      auto-save-default nil)
+  :config
+  (set-face-attribute 'default nil :height 150)
+  (menu-bar-mode -1)
+  (when (display-graphic-p)
+    (tool-bar-mode -1)
+    (scroll-bar-mode -1))
 
-(setq make-backup-files nil
-      auto-save-default nil)
+  :bind ("C-x c" . my/compile))
 
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(use-package project
+  :init (setq compilation-always-kill t))
+
+(use-package fido-mode
+  :hook (after-init . fido-mode))
+
+(use-package package
+  :config (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t))
 
 (use-package magit
   :ensure t
   :bind ("C-x g" . magit-status))
 
-(use-package fido-mode
-  :hook (after-init . fido-mode))
-
 (use-package almost-mono-themes
   :ensure t
   :config
-   (load-theme 'almost-mono-black t)
-   (load-theme 'almost-mono-white t))
+   (load-theme 'almost-mono-black t t)
+   (load-theme 'almost-mono-white t t))
 
 (use-package auto-dark
   :ensure t
@@ -32,3 +44,21 @@
   :ensure t
   :custom (which-key-idle-delay 0.5)
   :hook (after-init . which-key-mode))
+
+(use-package exec-path-from-shell
+  :ensure t
+  :init (exec-path-from-shell-initialize))
+
+(use-package corfu
+  :ensure t
+  :init (global-corfu-mode))
+
+(use-package eglot)
+
+(use-package go-mode
+  :ensure t
+  :hook ((go-mode . eglot-ensure)
+	 (go-mode . (lambda () (setq-local
+				tab-width 4
+				compile-command "go run ./...")))
+	 (before-save . gofmt-before-save)))
